ebeac6f3f454e90bdef3eab9c6dd05adb680c9fe
jpmsilvamessias <joaopedr901673@gmail.com>
2025-04-03 20:33:30 -0300

    Inicializado

---
+++ Rpg/Personagem.cs
@@ -0,0 +1,56 @@
+﻿namespace Rpg;
+
+public class Personagem
+{
+    private String nome;
+    private int vida = 100;
+    private List<int> potion = new List<int>{ 1, 2 };
+
+
+    public String Nome => nome;
+    
+    public Personagem(string nome)
+    {
+        this.nome = nome;
+    }
+
+
+    public int ataqueLeve()
+    {
+        Random random = new Random();
+        int acerto= random.Next(1, 10);
+        return acerto;
+    }
+
+    public int ataqueMedio()
+    {
+        Random random = new Random();
+        int acerto= random.Next(10, 20);
+        return acerto;
+    }
+
+    public int ataqueForte()
+    {
+        Random random = new Random();
+        int acerto= random.Next(20, 30);
+        return acerto;
+    }
+
+    public int defesa(int dano)
+    {
+        return dano / 2;
+    }
+
+    public void poti(List<int> a )
+    {
+        try
+        {
+            a.RemoveAt(0);
+            this.vida += 20;
+        }
+        catch (ArgumentOutOfRangeException e )
+        {
+            Console.WriteLine("Erro lista esra vazia"+e.Message);
+        }
+    }
+}
---
+++ Rpg/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Rpg;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        
+        Console.WriteLine("seja bem vindo ao Rpg");
+        Console.WriteLine("vamos criar seu personagem responda insira um nome");
+        string nome = Console.ReadLine();
+        Personagem personagem = new Personagem(nome);
+        Console.WriteLine($"seja bem vindo jogador{nome}  ");
+        Personagem inimigo = new Personagem("goblin");
+       Console.WriteLine($"{inimigo.Nome}");
+        
+        
+    }
+}
