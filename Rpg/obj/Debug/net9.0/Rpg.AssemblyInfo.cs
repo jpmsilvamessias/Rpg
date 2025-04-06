0e5e40b7affd6788b88dc5e0be509f5bb64b0d31
jpmsilvamessias <joaopedr901673@gmail.com>
2025-04-05 22:30:30 -0300

    finalizado

--- Rpg/obj/Debug/net9.0/Rpg.AssemblyInfo.cs
+++ Rpg/obj/Debug/net9.0/Rpg.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Rpg")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d611a0e0ea551e47bbe02c643f725fab047cb6b2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+492c12f0d5a1b326eefb4dc54d629a341004e111")]
 [assembly: System.Reflection.AssemblyProductAttribute("Rpg")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Rpg")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
